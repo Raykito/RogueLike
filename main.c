50c052def1b26f3ce0697ee230a4f6e974b097d1
Nathan Tual <nathan.tual@epitech.eu>
2016-10-31 22:45:14 +0100

    main

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for roguelike in /home/nathan/Perso/Games/RogueLike
+** 
+** Made by Nathan
+** Login   <nathan.tual@epitech.eu>
+** 
+** Started on  Mon Oct 31 22:43:12 2016 Nathan
+** Last update Mon Oct 31 22:43:47 2016 Nathan
+*/
+
+int	main()
+{
+  return (0);
+}
