a6c49ee9c95bf82ca5ec42b091b59eb0bec35c00
Nathan Tual <nathan.tual@epitech.eu>
2016-10-31 22:54:50 +0100

    maj_main

--- main.c
+++ main.c
@@ -5,10 +5,19 @@
 ** Login   <nathan.tual@epitech.eu>
 ** 
 ** Started on  Mon Oct 31 22:43:12 2016 Nathan
-** Last update Mon Oct 31 22:43:47 2016 Nathan
+** Last update Mon Oct 31 22:54:26 2016 Nathan
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 int	main()
 {
+  int	nb_room;
+
+  srand(time(NULL));
+  nb_room = rand() % 13;
+  printf("%d\n", nb_room);
   return (0);
 }
